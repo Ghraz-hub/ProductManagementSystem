4280187b1a604acd98598c2270f005b9ecb8fa5f
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-06 12:44:45 +0200

    Add product model

---
+++ ProductManagementSystem.App/Products/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductManagementSystem.App.Products.Models;
+
+public class Product(string name, decimal price, int quantity, DateTime logged)
+{
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+    public int Quantity { get; set; } = quantity;
+    public DateTime Logged { get; set; } = logged;
+}
