b8bd587c7c08407d518d0b093ed7eb1d6b073489
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-06 16:16:41 +0200

    Add SupplierService and Interface

---
+++ ProductManagementSystem.App/Suppliers/Services/SupplierService.cs
@@ -0,0 +1,19 @@
+﻿using ProductManagementSystem.App.Suppliers.Models;
+
+namespace ProductManagementSystem.App.Suppliers.Services;
+
+public class SupplierService : ISupplierService
+{
+    private readonly List<Supplier> _supplierList = [];
+    public Supplier CreateSupplier(string supplierName, string Location, string BuissnessNumber, string Email, string? PhoneNumber = null)
+    {
+        var supplier = new Supplier(supplierName, Location, BuissnessNumber, Email, PhoneNumber);
+
+        return supplier;
+    }
+
+    public List<Supplier> GetSuppliers()
+    {
+        return _supplierList;
+    }
+}
