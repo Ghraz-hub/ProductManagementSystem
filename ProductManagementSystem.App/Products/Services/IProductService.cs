bc8b2c29c5b1967484ef62fe6a7b4527b777c913
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-06 13:07:52 +0200

    Add IProductService and ProductService

---
+++ ProductManagementSystem.App/Products/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using ProductManagementSystem.App.Products.Models;
+
+namespace ProductManagementSystem.App.Products.Services;
+
+public interface IProductService
+{
+    public Product CreateProduct(string name, decimal price, int quantity);
+
+    public List<Product> GetAllProducts(List<Product> products); 
+}
---
+++ ProductManagementSystem.App/Products/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ProductManagementSystem.App.Products.Models;
+
+namespace ProductManagementSystem.App.Products.Services;
+
+internal class ProductService : IProductService
+{
+    private readonly List<Product> _productList = [];
+    public Product CreateProduct(string name, decimal price, int quantity)
+    {
+        var logged = DateTime.Now;
+
+        var newProduct = new Product(name, price, quantity, logged);
+
+        _productList.Add(newProduct);
+
+        return newProduct;
+    }
+
+    public List<Product> GetAllProducts(List<Product> products)
+    {
+        return _productList;
+    }
+}
