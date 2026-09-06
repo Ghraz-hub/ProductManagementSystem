b29fdb56d510be70f318ef8f3fb256dc42897e46
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-06 18:11:11 +0200

    Restore feature-main-menu files to main

---
+++ ProductManagementSystem.App/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProductManagementSystem.App.Customers.Dialog;
+using ProductManagementSystem.App.Customers.Services;
+using ProductManagementSystem.App.MainMenu;
+using ProductManagementSystem.App.Products.Dialog;
+using ProductManagementSystem.App.Products.Services;
+using ProductManagementSystem.App.Suppliers.Dialog;
+using ProductManagementSystem.App.Suppliers.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Customer
+builder.Services.AddSingleton<ICustomerService, CustomerService>();
+builder.Services.AddTransient<ICustomerDialog, CustomerDialog>();
+
+// Product
+builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Services.AddTransient<IProductDialog, ProductDialog>();
+
+// Supplier
+
+builder.Services.AddSingleton<ISupplierService, SupplierService>();
+builder.Services.AddTransient<ISupplierDialog, SupplierDialog>();
+
+// Bygg EFTER att allt registrerats
+using var host = builder.Build();
+
+var dialogCustomer = host.Services.GetRequiredService<ICustomerDialog>();
+
+var dialogProduct = host.Services.GetRequiredService<IProductDialog>();
+
+var dialogSupplier = host.Services.GetRequiredService<ISupplierDialog>();
+
+var mainMenu = new MainMenu(dialogCustomer, dialogProduct, dialogSupplier);
+
+mainMenu.ShowMainMenu();
+
